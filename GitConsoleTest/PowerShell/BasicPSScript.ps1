8f5cdff1ca559fbbc47e03294f778079f2900232
vjdeshmukhgit <vijay@intelifx.com>
2016-08-09 16:15:00 -0400

    importing...

--- GitConsoleTest/PowerShell/BasicPSScript.ps1
+++ GitConsoleTest/PowerShell/BasicPSScript.ps1
@@ -4,7 +4,17 @@ $directorypath = Split-Path $invocation.MyCommand.Path
 
 Write-Host "Path $directorypath" 
 $toolkit = $directorypath + "\Xrm.Framework.CI.PowerShell.dll"
-
+$fileToImport = $directorypath + "\PatchFollowUnfollow_1_0_0_0.zip"
 Import-Module $toolkit
 
 Write-Host "Toolkit imported"
+
+$importJobId = [guid]::NewGuid()
+
+
+$connectionString="Url=https://nhtsagmssdev.crm9.dynamics.com; Username=mark.chinstate@usdot.onmicrosoft.com; Password=Kiss12345!; authtype=Office365"
+
+
+$import = Import-XrmSolution -ImportAsync $false -ConnectionString $connectionString -PublishWorkflows $true  -SolutionFilePath $fileToImport -WaitForCompletion $true
+
+Write-Host "Import Process completed "
