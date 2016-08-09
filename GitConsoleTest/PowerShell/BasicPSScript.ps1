990966e636da845f15b35131a64c119dea5f9142
vjdeshmukhgit <vijay@intelifx.com>
2016-08-09 15:45:08 -0400

    importing xrm dll

--- GitConsoleTest/PowerShell/BasicPSScript.ps1
+++ GitConsoleTest/PowerShell/BasicPSScript.ps1
@@ -1,5 +1,10 @@
-﻿Write-Host "Hello World! 123"
+﻿Write-Host "Hello World! 122"
 $invocation = (Get-Variable MyInvocation).Value
 $directorypath = Split-Path $invocation.MyCommand.Path
 
 Write-Host "Path $directorypath" 
+$toolkit = $directorypath + "\Xrm.Framework.CI.PowerShell.dll"
+
+Import-Module $toolkit
+
+Write-Host "Toolkit imported"
