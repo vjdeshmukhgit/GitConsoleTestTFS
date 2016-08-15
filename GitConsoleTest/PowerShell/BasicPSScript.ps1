9c9c2df3c3a0e735baaf18f3838e3aea0f671705
vjdeshmukhgit <vijay@intelifx.com>
2016-08-15 16:17:19 -0400

    Added dynamic solution file selection

--- GitConsoleTest/PowerShell/BasicPSScript.ps1
+++ GitConsoleTest/PowerShell/BasicPSScript.ps1
@@ -4,17 +4,40 @@ $directorypath = Split-Path $invocation.MyCommand.Path
 
 Write-Host "Path $directorypath" 
 $toolkit = $directorypath + "\Xrm.Framework.CI.PowerShell.dll"
-$fileToImport = $directorypath + "\PostPatch_1_0_0_0.zip"
-Import-Module $toolkit
+$onlySolutionFilesPath = $directorypath + "\*.zip"
 
-Write-Host "Toolkit imported"
+# get file to import. Use the last Updated .zip (solution file)
+$fileToImport = "none"
+$files = Get-ChildItem -Path $onlySolutionFilesPath | Sort-Object LastWriteTime -Descending
+foreach($item in $files)
+{
+    $fileToImport =$item 
+	$writetime = $item.LastWriteTime
+    Write-Host "Importing file: $fileToImport; $writetime"
+    break
 
-$importJobId = [guid]::NewGuid()
+}
+# if solution file found then continue.
+if($fileToImport -ne "none")
+{
+    # continue with the build
+    Import-Module $toolkit
+
+    Write-Host "Toolkit imported"
+
+    $importJobId = [guid]::NewGuid()
 
 
-$connectionString="Url=https://nhtsagmssuat.crm9.dynamics.com; Username=mark.chinstate@usdot.onmicrosoft.com; Password=Kiss12345!; authtype=Office365"
+    $connectionString="Url=https://nhtsagmssuat.crm9.dynamics.com; Username=mark.chinstate@usdot.onmicrosoft.com; Password=Kiss12345!; authtype=Office365"
 
 
-$import = Import-XrmSolution -ImportAsync $false -ConnectionString $connectionString -PublishWorkflows $true  -SolutionFilePath $fileToImport -WaitForCompletion $true
+    $import = Import-XrmSolution -ImportAsync $false -ConnectionString $connectionString -PublishWorkflows $true  -SolutionFilePath $fileToImport -WaitForCompletion $true
+
+}
+else
+{
+    Write-Host "No Solution file found to import." -ForegroundColor Red -BackgroundColor White
+}
+#$fileToImport = $directorypath + "\PostPatch_1_0_0_0.zip"
 
 Write-Host "Import Process completed "
