b4847debfde140bb19d26722422301067bd2aaef
vjdeshmukhgit <vijay@intelifx.com>
2016-08-17 12:42:19 -0400

    Added logging

--- GitConsoleTest/PowerShell/CrmSolutionBuildScript.ps1
+++ GitConsoleTest/PowerShell/CrmSolutionBuildScript.ps1
@@ -10,16 +10,21 @@ $toolkit = $directorypath + "\Xrm.Framework.CI.PowerShell.dll"
 # get crm solutions folder
 $crmfolder = Split-Path -Parent $directorypath  
 $crmfolder = $crmfolder + "\CRMSolutions"
+$crmfolderSolutionFiles = $crmfolder + "\*.zip"
+
 Write-Host "CRM Folder Path:  $crmfolder" 
 
 # get file to import. Use the last Updated .zip (solution file)
-$fileToImport = "none"
-$files = Get-ChildItem -Path $crmfolder | Sort-Object LastWriteTime -Descending
+$FullPathOffileToImport = "none"
+$importFileName = ""
+$files = Get-ChildItem -Path $crmfolderSolutionFiles | Sort-Object LastWriteTime -Descending
 foreach($item in $files)
 {
-    $fileToImport =$item.FullName
+    $FullPathOffileToImport =$item.FullName
+    $importFileName = $item.Name
+
 	$writetime = $item.LastWriteTime
-    Write-Host "Importing file: $fileToImport; $writetime"
+    Write-Host "Importing file: $FullPathOffileToImport; $importFileName"
     break
 
 }
@@ -32,14 +37,70 @@ if($fileToImport -ne "none")
     Write-Host "Toolkit imported"
 
     $importJobId = [guid]::NewGuid()
+	$solutionInfo = Get-XrmSolutionInfoFromZip -SolutionFilePath $FullPathOffileToImport
+    
     $connectionString="Url=https://nhtsagmssuat.crm9.dynamics.com; Username=mark.chinstate@usdot.onmicrosoft.com; Password=Kiss12345!; authtype=Office365"
-    $import = Import-XrmSolution -ImportAsync $false -ConnectionString $connectionString -PublishWorkflows $true  -SolutionFilePath $fileToImport -WaitForCompletion $true
+    $import = Import-XrmSolution -ImportAsync $false -ConnectionString $connectionString -ImportJobId $importJobId -PublishWorkflows $true  -SolutionFilePath $FullPathOffileToImport -WaitForCompletion $true
+
+    #read import log
+    $logFile = $importFileName.Replace(".zip", "_" + [System.DateTime]::Now.ToString("yyyyMMdd_HH_mm"))
+    $importLogFile = $crmfolder + "\" + $logFile + "_Info.xml" 
+    $importJob = Get-XrmSolutionImportLog -ImportJobId $importJobId -ConnectionString $connectionString -OutputFile $importLogFile
+
+    $importProgress = $importJob.Progress
+    $importResult = (Select-Xml -Content $importJob.Data -XPath "//solutionManifest/result/@result").Node.Value
+    $importErrorText = (Select-Xml -Content $importJob.Data -XPath "//solutionManifest/result/@errortext").Node.Value
+
+    if (($importResult -ne "success") -or ($importProgress -ne 100))
+    {
+        throw "Import Failed. Error Text: $importErrorText"
+    }
+    else
+    {
+        Write-Host "Solution Imported"
+
+        #publish
+        Write-Host "Publishing..."
+        Publish-XrmCustomizations -ConnectionString $connectionString
+        Write-Host "Publishing complete"
+    }
+   
+    # Write the import results in xml
+
+    # this is where the document will be saved:
+    $xmlFile = $crmfolder + "\" + $logFile + "_ImportResult.xml"
+ 
+
+    # get an XMLTextWriter to create the XML
+    $XmlWriter = New-Object System.XMl.XmlTextWriter($xmlFile,$Null)
+ 
+    # choose a pretty formatting:
+    $xmlWriter.Formatting = 'Indented'
+    $xmlWriter.Indentation = 1
+    $XmlWriter.IndentChar = "`t"
+ 
+    # write the header
+    $xmlWriter.WriteStartDocument()
+    # set XSL statements
+    #$xmlWriter.WriteProcessingInstruction("xml-stylesheet", "type='text/xsl' href='style.xsl'")
+ 
+    # create root element "machines" and add some attributes to it
+    $XmlWriter.WriteComment('Imported Solution File Log')
+    $xmlWriter.WriteStartElement('Import')
+    $XmlWriter.WriteElementString('RunResult',$importResult)
+    $XmlWriter.WriteElementString('Error',$importErrorText)
+
+    $xmlWriter.WriteEndElement()
+ 
+     # finalize the document:
+    $xmlWriter.WriteEndDocument()
+    $xmlWriter.Flush()
+    $xmlWriter.Close()
 
 }
 else
 {
     Write-Host "No Solution file found to import." -ForegroundColor Red -BackgroundColor White
 }
-#$fileToImport = $directorypath + "\PostPatch_1_0_0_0.zip"
 
 Write-Host "Import Process completed "
